94ce7d7fcbde5cbfb4398819dd80d5d38b3fd102
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-29 16:00:30 +0100

    Create caption_tags table

--- main.sql
+++ main.sql
@@ -61,3 +61,12 @@ CREATE TABLE photo_tags (
 	
 	UNIQUE (user_id, post_id)
 );
+
+CREATE TABLE caption_tags (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+	
+	UNIQUE (user_id, post_id)
+);
